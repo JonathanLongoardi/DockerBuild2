4f63de9be8ce80b6ae6192de3e6e0047043dbb54
Jonathan Longobardi <64801320+JonathanLongoardi@users.noreply.github.com>
2021-12-16 17:01:20 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,27 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+var sqlze = require('sequelize');
+var mysql =  require('mysql');
+var db = new sqlze('id18136124_docker3', 'username', '\Tq27Yu4A]tXy(@5',{
+dialect: 'mysql',
+port: 3306
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('haciendo el sql...')
 })
 
 app.listen(process.env.PORT, '0.0.0.0', () => {
   //console.log(`Example app listening at http://localhost:${port}`)
 })
+
+db
+.authenticate()
+.complete(function(err){
+if(!!err) {
+console.log('Unable to connect to database: ', err);
+} else {
+console.log('Connection OK!');
+}
+});
