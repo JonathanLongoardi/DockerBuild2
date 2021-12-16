a3bcb67dae41d4f9f381efd6bf270e68a705d808
Jonathan Longobardi <64801320+JonathanLongoardi@users.noreply.github.com>
2021-12-16 17:12:09 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 var sqlze = require('sequelize');
 var mysql =  require('mysql');
