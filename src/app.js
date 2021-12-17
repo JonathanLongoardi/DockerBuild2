35dfaae39659f05ec5dd76ee0365180e2b9fe9ca
Jonathan Longobardi <64801320+JonathanLongoardi@users.noreply.github.com>
2021-12-17 10:54:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,51 +3,23 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 
-let jsonSql = require('json-sql');
-const promise = require('bluebird');
-const SqlConnection = require("tedious").Connection;
-const Request = require("tedious").Request;
 
-executeQuery(query); {
-        let resultEntity = {
-            result: {},
-            error: null
-        };
+var mysql = require('mysql');
 
-        return new promise((resolve, reject) => {
-            var connection = new SqlConnection(this.config);
+var con = mysql.createConnection({
+  host: "files.000webhost.com",
+  user: "docker3",
+  password: "%R^mdLzvl!WRH@Wf%Hwf",
+  database: "id18136124_docker3"
+});
 
-            connection.on('connect', function (err) {
-                let request = new Request(query, function (err, rowCount, rows) {
-                    if (err) {
-                        resultEntity.error = err;
-                        reject(resultEntity);
-                    } else {
-                        resultEntity.result = rows;
-                        resolve(resultEntity);
-                    }
-
-                    connection.close();
-                });
-                connection.execSql(request);
-            }
-           );
-        });
-    }
-
-
-
-
-
-
-
-
-module.exports = {
-  HOST: "files.000webhost.com",
-  USER: "docker3",
-  PASSWORD: "%R^mdLzvl!WRH@Wf%Hwf",
-  DB: "id18136124_docker3"
-};
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM Tabla1", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 
 
