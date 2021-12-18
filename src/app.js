2e8176597e0168d78b42dd5ff8c1a959e430bed2
ipmSandovalIgnacio <88504703+ipmSandovalIgnacio@users.noreply.github.com>
2021-12-17 23:25:06 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,15 +7,16 @@ const PORT = process.env.PORT || 3000;
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "files.000webhost.com",
-  user: "docker3",
-  password: "%R^mdLzvl!WRH@Wf%Hwf",
-  database: "id18136124_docker3"
+  host: "remotemysql.com",
+  user: "QQIfU2UnGp",
+  password: "0daqjLMrOS",
+  database: "QQIfU2UnGp"
 });
+
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT * FROM Tabla1", function (err, result, fields) {
+  con.query("select * from TABLA1", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
@@ -24,10 +25,11 @@ con.connect(function(err) {
 
 
 
+
 app.get('/', (req, res) => {
   res.send('haciendo el sql...')
 })
 
-app.listen(process.env.PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
